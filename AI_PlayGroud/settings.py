60a2ad1e750cda7230a7ca9096f1dabb8a74282a
LeoRY <59327886+Lw-CodeStorage@users.noreply.github.com>
2023-01-22 14:55:28 +0800

    測試whitenoise + uvicorn 啟動於gcp

--- AI_PlayGroud/settings.py
+++ AI_PlayGroud/settings.py
@@ -66,11 +66,10 @@ MIDDLEWARE = [
     "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',  
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
 ]
 
 ROOT_URLCONF = 'AI_PlayGroud.urls'
